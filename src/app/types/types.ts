a57a4b640a3641e75190213164d6c547c36a4d39
Jadson Lima <jadsonmath874@gmail.com>
2025-09-06 20:41:27 -0400

    refactor - types.ts

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -1,27 +1,3 @@
-export interface Person {
-  id: string;
-  name: string;
-  age: number;
-  photo: string;
-  status: 'missing' | 'found';
-  lastSeen: {
-    date: string;
-    location: string;
-  };
-  description: string;
-  characteristics: {
-    height: string;
-    weight: string;
-    eyeColor: string;
-    hairColor: string;
-  };
-  contactInfo: {
-    phone: string;
-    email: string;
-  };
-  createdAt: string;
-}
-
 export interface Information {
   id: string;
   personId: string;
@@ -31,4 +7,59 @@ export interface Information {
   location?: string;
   photos?: string[];
   createdAt: string;
+}
+
+interface OcorrenciaEntrevDesapDTO {
+  informacao: string;
+  vestimentasDesaparecido: string;
+}
+
+interface UltimaOcorrencia {
+  dtDesaparecimento: string;
+  dataLocalizacao: string | null;
+  encontradoVivo: boolean;
+  localDesaparecimentoConcat: string;
+  ocorrenciaEntrevDesapDTO?: OcorrenciaEntrevDesapDTO;
+  ocoId: number;
+}
+
+export interface PessoaDTO {
+  id: number;
+  nome: string;
+  idade: number;
+  sexo: "MASCULINO" | "FEMININO";
+  vivo: boolean;
+  urlFoto: string;
+  ultimaOcorrencia: UltimaOcorrencia;
+}
+
+export interface Pageable {
+  pageNumber: number;
+  pageSize: number;
+  offset: number;
+  unpaged: boolean;
+  paged: boolean;
+  sort: {
+    sorted: boolean;
+    unsorted: boolean;
+    empty: boolean;
+  };
+}
+
+export interface PessoaPaginadaResponse {
+  content: PessoaDTO[];
+  empty: boolean;
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  pageable: Pageable;
+  size: number;
+  sort: {
+    sorted: boolean;
+    unsorted: boolean;
+    empty: boolean;
+  };
+  totalElements: number;
+  totalPages: number;
 }
